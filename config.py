239261a114ede66147ea2f05bf123cde102d27ed
z-zone <37634587+z-zone@users.noreply.github.com>
2018-03-21 20:50:30 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+# config.py
+# authors: lukas, samuel
+
+# config.py is to define all the site-specific parameters, that are too sensible to store in github
+# the idea is to build a short setup-sequence that asks for the parameters while installation.
+
+# ssid of local wlan
+ssid = ""
+
+# pre shared key for local wlan
+psk = ""
+
+# server-adress of the server-deamon
+serv = "localhost"
+
+# port of socket-listener in server-deamon
+port = "5112"
+
+# private key for string-encryption
+priv = ""
