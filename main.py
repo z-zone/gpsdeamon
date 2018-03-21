afeb9ee36745c73e7499d8b887df9b60d3ee6c27
z-zone <37634587+z-zone@users.noreply.github.com>
2018-03-21 19:55:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # main.py
 # authors: lukas, samuel
 
-# in the con.py-module (connections) the different types of possible connections (gsm, wlan) are defined
+# in the connect.py-module (connections) the different types of possible connections (gsm, wlan) are defined
 
 # in the gps.py-module the communication with the gps-module is defined
 
-# in the par.py-module (parse) the handling of data-string is defined either the client- or the server-way
+# in the parse.py-module (parse) the handling of data-string is defined either the client- or the server-way
 
 # in the talk.py-module the sending/recieving takes place
 
