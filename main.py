9d0eab56f1e46b4b79bce505bee345b2f2834a19
z-zone <37634587+z-zone@users.noreply.github.com>
2018-03-21 19:48:58 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+# main.py
+# authors: lukas, samuel
+
+# first, the gpsdeamon should decide, if it's the client or the server.
+# therefore it should check if the gps-module is installed -> client, or not -> server
+
+
