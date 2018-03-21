c607c9662d862a92f88909da3fd5a82e9c1cd4cc
z-zone <37634587+z-zone@users.noreply.github.com>
2018-03-21 19:53:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 # main.py
 # authors: lukas, samuel
 
+# in the con.py-module (connections) the different types of possible connections (gsm, wlan) are defined
+
+# in the gps.py-module the communication with the gps-module is defined
+
+# in the par.py-module (parse) the handling of data-string is defined either the client- or the server-way
+
+# in the talk.py-module the sending/recieving takes place
+
 # first, the gpsdeamon should decide, if it's the client or the server.
 # therefore it should check if the gps-module is installed -> client, or not -> server
-
 
