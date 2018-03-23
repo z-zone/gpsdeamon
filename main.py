23c54a9cef6ff37d9c8728baaa1d87488f4911a7
z-zone <37634587+z-zone@users.noreply.github.com>
2018-03-23 20:17:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,21 @@
 
 # the config.py defines all the parameters
 import config
-# in the connect.py-module (connections) the different types of possible connections (gsm, wlan) are defined
-import connect
 # in the gps.py-module the communication with the gps-module is defined
 import gps
-# in the parse.py-module (parse) the handling of data-string is defined either the client- or the server-way
-import parse
-# in the talk.py-module the sending/recieving takes place
-import talk
 
 # first, the gpsdeamon should decide, if it's the client or the server.
 # therefore it should check if the gps-module is installed -> client, or not -> server
 instType = "s"
 if gps.isInstalled() == true:
     instType = "c"
+
+# in the parse.py-module (parse) the handling of data-string is defined either the client- or the server-way
+import parse
+# in the connect.py-module (connections) the different types of possible connections (gsm, wlan) are defined
+import connect
+# in the talk.py-module the sending/recieving takes place
+import talk
+
 
 # establish the connection to the other part
