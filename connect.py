e30096f0f5981a11ce0848dbc1b0443a79111b9e
z-zone <37634587+z-zone@users.noreply.github.com>
2018-03-23 20:34:58 +0100

    Update connect.py

--- connect.py
+++ connect.py
@@ -2,8 +2,16 @@
 # authors: lukas, samuel
 
 if instType == "s":
-    pass
+    ssock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ('', 5112)
+    sock.bind(server_address)
+    sock.listen(5)
+    while 1:
+        connection, client_address = ssock.accept()
 elif instType == "c":
-    pass
+    csock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # specify the address of server - in final version the dyndnsname should be here, but not shown to everyone now
+    server_address = (sys.argv[2], 5112)
+    csock.connect(server_adress)
 else
     raise RuntimeError("Installation Type not specified")
