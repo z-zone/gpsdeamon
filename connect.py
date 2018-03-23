4082feade51842abe655a31ea3bd19207d064349
z-zone <37634587+z-zone@users.noreply.github.com>
2018-03-23 21:26:49 +0100

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,17 +1,51 @@
 # connect.py
 # authors: lukas, samuel
 
-if instType == "s":
-    ssock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = ('', 5112)
-    sock.bind(server_address)
-    sock.listen(5)
-    while 1:
-        connection, client_address = ssock.accept()
-elif instType == "c":
-    csock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # specify the address of server - in final version the dyndnsname should be here, but not shown to everyone now
-    server_address = (sys.argv[2], 5112)
-    csock.connect(server_adress)
-else
-    raise RuntimeError("Installation Type not specified")
+def communicate():
+    # define the specific length of a possible message
+    msglen = 23
+    # server version
+    if instType == "s":
+        handler = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = ('', 5112)
+        handler.bind(server_address)
+        handler.listen(5)
+        while 1:
+            ssock, client_address = handler.accept()
+        try:
+            bytes_recd = 0
+            buffers = []
+            while bytes_recd < msglen:
+                buffer = ssock.recv(min(MSGLEN - bytes_recd, 2048))
+                if buffer == '':
+                    raise RuntimeError("server lost socket connection")
+                else:
+                    buffers.append(buffer)
+                    bytes_recd += len(buffer)
+            # return the full received message
+            return ''.join(buffers)
+        finally:
+            ssock.close()
+    # client version
+    elif instType == "c":
+        csock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # specify the address of server - in final version the dyndnsname should be here, but not shown to everyone now
+        server_address = (sys.argv[2], 5112)
+        csock.connect(server_adress)
+        try:
+            # send data
+            # message just for testing set to argv[3]
+            msgstring = sys.argv[3]
+            totalsent = 0
+            while totalsent < msglen:
+                sent = csock.send(msgstring[totalsent:])
+                if sent == 0:
+                    raise RuntimeError("client lost socket connection")
+                totalsent += sent
+            return true
+        finally:
+            csock.close
+        
+    # no version specified
+    else
+        raise RuntimeError("Installation Type not specified")
